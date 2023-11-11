9530c5d966b58365a9358cf39df7c8f6b2310695
csl12138 <714787861@qq.com>
2023-11-11 23:21:02 +0800

    feat: 整理代码

--- src/PageA/index.js
+++ src/PageA/index.js
@@ -2,15 +2,10 @@ import sync from './sync';
 import css from './index.css';
 console.log(css, sync);
 
-function changePublish(p) {
-    p && (__webpack_require__.p = p);
-};
 
-window.__changePublish = changePublish;
 
 const div = document.getElementById('main');
 div.onclick = async () => {
-    // window.__changePublish('https://static.pddpic.com');
     const module = await import(/* webpackChunkName: "async.js" */ './async.js');
     console.log('😎😎😎 ~ div.onclick= ~ module:', module);
     // const css = await import(/* webpackChunkName: "async.css" */ './async.css');
